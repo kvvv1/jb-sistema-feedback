a809472e9a9779cab49a817766276e8020a852cf
kvvv1 <kaikevns1@gmail.com>
2025-07-04 12:29:23 -0300

    a

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const sql = require('mssql');
-const cors = require('cors');
+import express from 'express';
+import sql from 'mssql';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
